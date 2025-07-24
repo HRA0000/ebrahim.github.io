c0e18ff13c1742ca1cff37dcd9e710364d39ab2a
ebrahimtabib <ebrahemaltbeb@gmail.com>
2025-07-24 15:30:21 +0200

    Fix Vite entry and prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ebrahim.github.io/', // <-- must match your repo name
+  base: '/ebrahim.github.io/',
   plugins: [react()],
 })
